5be737d45ed54d06669b7d6b12615e6cc3ae147b
Israel Carlebach <icarlebach@gmail.com>
2023-12-25 21:10:17 -0500

    Testing commit

--- src/ExpresionsAndStatements/Part1.java
+++ src/ExpresionsAndStatements/Part1.java
@@ -3,5 +3,6 @@ package ExpresionsAndStatements;
 public class Part1 {
     public static void main(String[] args) {
         System.out.println("write lesson code here");
+        System.out.println("write lesson code here");
     }
 }
