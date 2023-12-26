876b37abb70fcf33324c0eadf9de60f6e62d34c1
Israel Carlebach <icarlebach@gmail.com>
2023-12-26 12:49:42 -0500

    Challenge using to methods to display a message. Note how I did it vs. how the instructor did it

---
+++ src/ExpresionsAndStatements/MethodChallenge.java
@@ -0,0 +1,40 @@
+package ExpresionsAndStatements;
+
+public class MethodChallenge {
+    public static void main(String[] args) {
+
+        //My code
+
+        displayHighScorePosition("Mendel", calculateHighScorePosition(1500));
+        displayHighScorePosition("Jo", calculateHighScorePosition(1000));
+        displayHighScorePosition("Chaim", calculateHighScorePosition(500));
+        displayHighScorePosition("Fred", calculateHighScorePosition(100));
+        displayHighScorePosition("Sarah", calculateHighScorePosition(25));
+
+        //Instructors method
+
+//        int position = calculateHighScorePosition(1500);
+//        displayHighScorePosition("Sarah", position);
+
+        //repeating 5x with different names and scores
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position " +
+                playerPosition +
+                " on the high score list.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        }
+        return 4;
+    }
+}
