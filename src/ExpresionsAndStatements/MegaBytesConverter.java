0700f0117f507f09ba46300edf7a30ec9f80637f
Israel Carlebach <icarlebach@gmail.com>
2023-12-28 14:16:12 -0500

    Coding Exercise 3: Converts Kb to MB.

---
+++ src/ExpresionsAndStatements/MegaBytesConverter.java
@@ -0,0 +1,17 @@
+package ExpresionsAndStatements;
+
+public class MegaBytesConverter {
+    // write code here
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+
+        int remainder = kiloBytes % 1024;
+        int megaBytes = kiloBytes  / 1024;
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+        }
+    }
+}
