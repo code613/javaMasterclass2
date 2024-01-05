4f869eebc6da6e0a1eaf014965ea3506669b8a50
Israel Carlebach <icarlebach@gmail.com>
2024-01-05 15:28:40 -0500

    small changes. just commiting so i can checkout expressions and statements

--- src/Control_Flow/Switch.java
+++ src/Control_Flow/Switch.java
@@ -21,5 +21,16 @@ public class Switch {
                 break;
 
         }
+        String month = "October";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "January", "February", "March" -> "1st";
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "November", "December" -> "4th";
+            default -> "Bad";
+        };
     }
 }
