46dbd3998a8b4884f311e6323a4e2c875326ffa8
Israel Carlebach <icarlebach@gmail.com>
2024-01-09 22:15:08 -0500

    TheForLoop: Basic for loop and how to break out if a condition is met.

---
+++ src/Control_Flow/TheForLoop.java
@@ -0,0 +1,24 @@
+package Control_Flow;
+
+public class TheForLoop {
+
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+        for (double rate = 7.5; rate <= 10; rate += .25) {
+            double interestAmount = calculateInterest(100, rate);
+            if (interestAmount > 8.5) {
+                break;
+            }
+
+            System.out.println("100 at " + rate + "% interest = " + interestAmount);
+        }
+    }
+
+        public static double calculateInterest ( double amount, double interestRate){
+
+            return (amount * (interestRate / 100));
+        }
+
+}
