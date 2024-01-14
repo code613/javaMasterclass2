cab00e70128e1a1da225375768766260c249934d
Israel Carlebach <icarlebach@gmail.com>
2024-01-13 19:45:27 -0500

    Sum3And5Challenge: Put in wrong commit message last time. Also named variables so that the code can be understood better instead of having to comment

--- src/Control_Flow/Sum3And5Challenge.java
+++ src/Control_Flow/Sum3And5Challenge.java
@@ -5,21 +5,21 @@ public class Sum3And5Challenge {
     // evenly, adds those numbers together. Stops after it finds 3 numbers.
     public static void main(String[] args) {
         // will be used in the for loop to calculate sum
-        int sum = 0;
-        // variable will be used to stop for loop after three numbers are found
-        int counter = 0;
-        for (int i = 1; i <=1000; i++) {
+        int sumOfMatches = 0;
+        // variable will be used to stop for loop after five numbers are found
+        int countOfMatches = 0;
+        for (int loopNumber = 1; loopNumber <=1000; loopNumber++) {
 
-            if (i % 3 == 0 && i % 5 == 0) {
-                sum += i;
-                System.out.println(i);
-                counter++;
-                if (counter == 5) {
+            if (loopNumber % 3 == 0 && loopNumber % 5 == 0) {
+                sumOfMatches += loopNumber;
+                System.out.println(loopNumber);
+                countOfMatches++;
+                if (countOfMatches == 5) {
                     break;
                 }
             };
         }
-        System.out.println("Sum: " + sum);
+        System.out.println("Sum: " + sumOfMatches);
     }
         
 
